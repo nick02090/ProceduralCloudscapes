783def46b7c704cf9bc5da5e2dab6e68ac720f7b
Nikola Ćališ <nidjolas.u.b@gmail.com>
2021-05-27 16:20:58 +0200

    Show only skybox in the skybox test scene

--- ProceduralCloudscapes/ProceduralCloudscapes/Scenes/SkyboxTestScene.cpp
+++ ProceduralCloudscapes/ProceduralCloudscapes/Scenes/SkyboxTestScene.cpp
@@ -1,13 +1,7 @@
 #include "SkyboxTestScene.h"
 
-#include "../SceneObjects/Clouds.h"
-
 SkyboxTestScene::SkyboxTestScene(Window* _window) : Scene(_window, "Skybox Test Scene", EnvironmentType::Skybox)
 {
-	// Add clouds to the scene
-	Clouds* clouds = new Clouds(window);
-	addSceneObject(clouds);
-
 	// Set higher movement speed for the camera
 	window->getCamera()->setMovementSpeed(5000.f);
 }
